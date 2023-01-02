a8f389f4a734614bec445c2313ec665d6019d187
dac <dac@dypm.org>
2023-01-02 14:34:39 +0530

    changes

---
+++ juber229158/server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+var path=require("path");
+var fs=require("fs");
+
+
+//server configuration
+app.use(express.static(path.join(__dirname,'public')));
+
+ app.get('/', function (req, res) {
+   res.sendFile(path.join(___dirname + '/index.html'));
+}); 
+var server = app.listen(8081, function () {
+    console.log("server is running on port 8081")
+})
